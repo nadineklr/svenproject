c6199292031899b132ad5157f4111faefe8a0476
nadineklr <nadineklr@outlook.com>
2021-09-22 20:12:56 +0200

    add api

--- js/code.js
+++ js/code.js
@@ -2,10 +2,12 @@
 $.ajax({    // modernste Version
   url: 'https://cat-fact.herokuapp.com/facts'
 }).done( data => {
-  console.log(data)
+  console.log(data.text)
 
 
-  
+  let fact = data.text;
+
+  $('.fact-box').innerText = fact
 
   
 
